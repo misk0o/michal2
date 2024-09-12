69970d18dbc4faea4a0f35524c7e10a11f3a84ba
Peter <peter.sefcik777@gmail.com>
2024-09-12 10:24:13 +0200

    uvodny commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Typography from "@mui/material/Typography";
+
+export default function Home() {
+  return (
+    <Typography variant="h1" gutterBottom>
+        Hello world
+      </Typography>
+  );
+}
