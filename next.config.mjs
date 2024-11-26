442a0dd11b7eb374cade4541402846bcfaab1974
Peter <peter.sefcik777@gmail.com>
2024-11-26 21:16:16 +0100

    rada

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+  };
+  
+  export default nextConfig;
